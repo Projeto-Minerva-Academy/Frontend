307166c8cec90c28f3b62aa45d003bce3fe20c64
julia <julialameiras@gmail.com>
2024-08-27 17:15:04 -0300

    :sparkles: feat: routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Contato from "./pages/contato/Contato";
+import Sobre from "./pages/sobre/Sobre";
+import Footer from "./components/footer/Footer";
+
 function App() {
-  // Código TypeScript
 
   return (
-    // Código TSX => HTML + CSS
-    <> 
 
-      
+    <>
 
+    <BrowserRouter>
+
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/sobre" element={<Sobre />} />
+    <Route path="/contato" element={<Contato />} />
+    </Routes>
+
+    <Footer/>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
