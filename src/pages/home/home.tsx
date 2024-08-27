bac54de0adc8d2f99ba8f47e3afcd98abcf7c9dc
Pedro Barboza <peh123barboza@gmail.com>
2024-08-27 17:28:09 -0300

    feat: add Componets, Pages and Routes

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,57 +1,8 @@
 
 function Home() {
-
-    return (
-        <div className="flex justify-center bg-gray-200 ">
-            <div className="container grid grid-cols text-black " >
-                <div className="flex flex-col items-center justify-center gap-4 py-20 ">
-                    <h2 className="text-5xl font-bold ">
-                        Na Minerva, você dá o primeiro passo em direção a uma jornada de aprendizado transformadora!</h2>
-                    <p className="text-xl">
-                        Acreditamos que a educação é a chave para abrir portas e criar oportunidades,
-                        proporcionando a cada indivíduo as ferramentas necessárias para alcançar seu potencial máximo</p>
-
-                    <div className="flex justfy-center mt-10 gap-3">
-                        <div className="flex justify-around gap-4 ">
-                            <div className="px-4 py-2 border-2 border-black border-solid rounded-md ">
-                                Programação
-                            </div>
-                        </div>
-
-                        <div className="flex justify-around gap-4 ">
-                            <div className="px-4 py-2 border-2 border-black border-solid-md ">
-                                Designer
-                            </div>
-                        </div>
-
-                        <div className="flex justify-around gap-4 ">
-                            <div className="px-4 py-2 border-2 border-black border-solid rounded-md ">
-                                Banco de Dados
-                            </div>
-                        </div>
-
-                        <div className="flex justify-around gap-4 ">
-                            <div className="px-4 py-2 border-2 border-black border-solid rounded-md ">
-                                Gestão de Projetos
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="flex justify-around gap-4 ">
-                            <div className="px-4 py-2 border-2 border-black border-solid rounded-md ">
-                                Conteúdos Educacionais
-                            </div>
-                        </div>
-
-                </div>
-
-                <div className="flex justify-center ">
-
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div>Home</div>
+  )
 }
 
 export default Home
