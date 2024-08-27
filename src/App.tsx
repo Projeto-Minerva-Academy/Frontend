f03dde3c97a0d8ed900016b9b30363face900266
Pedro Barboza <peh123barboza@gmail.com>
2024-08-27 17:44:44 -0300

    feat: add Components, Pages and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-import Home from "./pages/home/home"
-import Sobre from "./pages/sobre/sobre"
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Contato from "./pages/contato/Contato";
 import Sobre from "./pages/sobre/Sobre";
 import Footer from "./components/footer/Footer";
->>>>>>> 307166c8cec90c28f3b62aa45d003bce3fe20c64
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    // Código TSX => HTML + CSS
-    <>
 
-      <Home />
-      <Sobre />
-=======
 
     <>
 
@@ -33,9 +22,8 @@ function App() {
 
     <Footer/>
     </BrowserRouter>
->>>>>>> 307166c8cec90c28f3b62aa45d003bce3fe20c64
     </>
   );
 }
 
-export default App
+export default App;
