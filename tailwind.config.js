afe9292a04b6b190580972447b2badeceb7cce27
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-30 12:12:17 -0300

    chore: install tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#003E6C', // Azul escuro
-        secondary: '#00A3E0', // Azul claro
-        accent: '#007A7A', // Verde acentuado
-        neutral: '#F4F7F9', // Cor de fundo neutra
+        primary: "#003E6C", // Azul escuro
+        secondary: "#00A3E0", // Azul claro
+        accent: "#007A7A", // Verde acentuado
+        neutral: "#F4F7F9", // Cor de fundo neutra
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], // Fonte principal
+        sans: ["Inter", "sans-serif"], // Fonte principal
       },
       boxShadow: {
-        default: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        md: '0 6px 12px rgba(0, 0, 0, 0.1)',
+        default: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        md: "0 6px 12px rgba(0, 0, 0, 0.1)",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
