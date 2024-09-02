959b0b97b92c32f2a1889fbc2e94b42668ee3a83
Roger Vinicius <rogervtsc@hotmail.com>
2024-09-02 18:17:37 -0300

    refac: footer allign items

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 text-gray-800 py-8 rounded-2xl shadow-md mx-auto max-w-screen-xl">
+    <footer className="bg-gray-200 text-gray-800 py-8 rounded-2xl shadow-md mx-auto max-w-screen-2xl ">
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap justify-center">
-          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+        <div className="flex justify-between text-center">
+          <div className="w-full md:w-1/3 mb-6 md:mb-0 ">
             <h2 className="text-xl font-bold mb-4">Sobre Nós</h2>
-            <p>Somos uma plataforma de cursos online na área de tecnologia 
-             com diversos cursos.</p> 
-             <p>
-              Te preparamos do zero ao avançado para você se tornar um profissional completo.
-            </p>
+              <div className="tracking-wide">
+                <p>Somos uma plataforma de cursos online na área de tecnologia
+                 com diversos cursos.</p>
+                 <p>
+                  Te preparamos do zero ao avançado para você se tornar um profissional completo.
+                </p>
+              </div>
           </div>
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h2 className="text-xl font-bold mb-4">Links Rápidos</h2>
